758e84883646a22a0e6091eb4ad3cec4487cf607
ivinaconsultancyinc <ivinaconsultancyinc@gmail.com>
2025-06-25 03:55:50 +0000

    Update Mortality_laps.py

--- cpflask_app_project/Mortality_laps.py
+++ cpflask_app_project/Mortality_laps.py
@@ -1,24 +1,45 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
-def simulate_policy_data():
+def simulate_mortality_lapse_data():
     np.random.seed(42)
     dates = pd.date_range(start='2015-01-01', periods=365*5, freq='D')
-    cash_flows = np.random.poisson(lam=100, size=len(dates))
-    return dates, cash_flows
+    age = np.random.randint(18, 70, size=len(dates))
+    policy_duration = np.random.randint(1, 30, size=len(dates))
+    mortality = np.random.binomial(1, 0.01, size=len(dates))
+    lapse = np.random.binomial(1, 0.05, size=len(dates))
+    data = pd.DataFrame({'ds': dates, 'age': age, 'policy_duration': policy_duration, 'mortality': mortality, 'lapse': lapse})
+    return data
 
-def calculate_reserves(dates, cash_flows, discount_rate):
-    reserves = np.zeros(len(dates))
-    for i in range(len(dates)):
-        reserves[i] = np.sum(cash_flows[i:] / (1 + discount_rate)**np.arange(1, len(dates)-i+1))
-    return reserves
+def train_mortality_model(data):
+    X_mortality = data[['age', 'policy_duration']]
+    y_mortality = data['mortality']
+    mortality_model = LogisticRegression()
+    mortality_model.fit(X_mortality, y_mortality)
+    return mortality_model
 
-def plot_reserves(dates, reserves):
+def train_lapse_model(data):
+    X_lapse = data[['age', 'policy_duration']]
+    y_lapse = data['lapse']
+    lapse_model = LogisticRegression()
+    lapse_model.fit(X_lapse, y_lapse)
+    return lapse_model
+
+def predict_mortality_lapse(data, mortality_model, lapse_model):
+    X_mortality = data[['age', 'policy_duration']]
+    X_lapse = data[['age', 'policy_duration']]
+    data['mortality_pred'] = mortality_model.predict_proba(X_mortality)[:, 1]
+    data['lapse_pred'] = lapse_model.predict_proba(X_lapse)[:, 1]
+    return data
+
+def plot_mortality_lapse(data):
     plt.figure(figsize=(10, 6))
-    plt.plot(dates, reserves, label='Reserves')
-    plt.title('Policy Reserves Over Time')
+    plt.plot(data['ds'], data['mortality_pred'], label='Mortality Prediction')
+    plt.plot(data['ds'], data['lapse_pred'], label='Lapse Prediction')
+    plt.title('Mortality and Lapse Predictions Over Time')
     plt.xlabel('Date')
-    plt.ylabel('Reserve Amount')
+    plt.ylabel('Probability')
     plt.legend()
     plt.show()
